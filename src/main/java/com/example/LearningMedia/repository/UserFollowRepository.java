903e54089b12c5b2e6c6eae17fedd7d677ec5eb8
Pramod Wijenayake <wijesinghawaspd@gmail.com>
2025-05-05 16:33:25 +0530

    Update UserFollowRepository.java

--- src/main/java/com/example/LearningMedia/repository/UserFollowRepository.java
+++ src/main/java/com/example/LearningMedia/repository/UserFollowRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+// User follow repository
 @Repository
 public interface UserFollowRepository extends MongoRepository<UserFollow, String> {
     
